e6f4b798807d36368cb1f9bc084c41de1b67afc3
gyyst <lantianhaian@hotmail.com>
2023-03-09 23:07:59 +0800

    a

---
+++ src/main/java/com/gyyst/myossgateway/MyOssGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gyyst.myossgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOssGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOssGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gyyst/myossgateway/MyOssGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyyst.myossgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOssGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
